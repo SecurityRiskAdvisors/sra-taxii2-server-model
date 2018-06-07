5cd4438b59a91f8c3440cbe22126dc7060efe480
thebleucheese <30511683+thebleucheese@users.noreply.github.com>
2018-06-07 11:22:48 -0400

    add more apiroot detail

--- models/apiroot.js
+++ models/apiroot.js
@@ -8,6 +8,9 @@ const Schema = mongoose.Schema;
 
 let ApiRootSchema = new Schema({
     name: { type: String, lowercase: true, required: true },
+    description: String,
+    contact: String,
+    default: Boolean
 }, {
     timestamps: true
 });
